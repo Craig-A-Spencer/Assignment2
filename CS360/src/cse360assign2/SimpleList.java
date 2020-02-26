babbb733880c034a31575ab91ac48c0f88ffc0ee
Craig Spencer <craig.spencer.github@gmail.com>
2020-02-25 19:27:29 -0700

    Updated the add and remove functions to be able to increase or decrease the size of the list array.
    Added the method append() which adds an element to the end of the list
    instead of the beginning.
    
    Added getter methods first() which returns the first element of the the
    list and size() which returns the current size of the list array.

--- CS360/src/cse360assign2/SimpleList.java
+++ CS360/src/cse360assign2/SimpleList.java
@@ -2,79 +2,96 @@ package cse360assign2;
 
 /* Name: Craig Spencer
  * Class ID: 126
- * Assignment: 1
+ * Assignment: 2
  * Description:
  * This class creates a array list with simple functions that allow the user to
- * manipulate this short integer array. This list can also be return to the user 
+ * manipulate this short integer array. This list can also be returned to the user 
  * as a String using the toString() method.
+ * 
+ * GitHub URL: https://github.com/Craig-A-Spencer/Assignment2.git
 */
 
-public class SimpleList
-{
+public class SimpleList {
 
 	//Instance variables
 	private int[] list;
-	private int count;
-	final int LIST_SIZE = 10;
+	private int count, currentListSize;
+	
+	//Constants
+	final int INITIAL_SIZE = 10;
+	final double INCREASE_AMT = 1.5;
+	final double DECREASE_AMT = 0.75;
 	
 	//Constructor for the SimpleList class
-	SimpleList ()
-	{
-		list = new int[LIST_SIZE];
+	SimpleList () {
+		currentListSize = INITIAL_SIZE;
+		list = new int[currentListSize];
 		count = 0;
 	}
 	
 	//Adds a new number to the list
-	public void add(int newNum)
-	{
-		for (int index = LIST_SIZE - 2; index >= 0; index--)
-		{
+	public void add(int newNum) {
+		//Increases the size of the array if needed
+		if (count == currentListSize) {
+			currentListSize *= INCREASE_AMT;
+			int[] tempList = new int[currentListSize];
+			
+			for (int index = 0; index < count; index++) {
+				tempList[index] = list[index];
+			}
+			
+			list = tempList;
+		}
+		
+		for (int index = currentListSize - 2; index >= 0; index--) {
 			list[index + 1] = list[index];
 		}
 		
 		list[0] = newNum;
-		
-		if (count < LIST_SIZE)
-		{
-			count++;
-		}
+		count++;
 	}
 	
 	//Removes a specified element from the list
-	public void remove(int num)
-	{
+	public void remove(int num) {
 		int position = search(num);
 		
-		while (position != -1)
-		{
-			for (int index = position; index < LIST_SIZE - 1; index++)
-			{
+		while (position != -1) {
+			for (int index = position; index < currentListSize - 1; index++) {
 				list[index] = list[index + 1];
 			}
 			
-			list[LIST_SIZE - 1] = 0;
+			list[currentListSize - 1] = 0;
 			count--;
 			position = search(num);
+		}
+		
+		//Decreases the size of the array if needed
+		if (count < currentListSize * DECREASE_AMT) {
+			currentListSize *= DECREASE_AMT;
+			
+			int[] tempList = new int[currentListSize];
+			
+			for (int index = 0; index < count; index++) {
+				tempList[index] = list[index];
+			}
+			
+			list = tempList;
 		}
 	}
 	
 	//Getter method for the count variable
-	public int count()
-	{
+	public int count() {
 		return count;
 	}
 	
 	//Returns a formated string version of the list array
-	public String toString()
-	{
+	public String toString() {
 		String output = "";
 		
-		for (int index = 0; index < count; index++)
-		{
+		for (int index = 0; index < count; index++) {
 			output += list[index];
 			
-			if (index < count - 1)
-			{
+			if (index < count - 1) {
 				output += " ";
 			}
 		}
@@ -83,11 +100,9 @@ public class SimpleList
 	}
 	
 	//Gives the first index of a specified number
-	public int search(int num)
-	{
+	public int search(int num) {
 		int position = -1;
-		for (int index = count - 1; index >= 0; index--)
-		{
+		for (int index = count - 1; index >= 0; index--) {
 			if (list[index] == num)
 				position = index;
 		}
@@ -95,10 +110,56 @@ public class SimpleList
 		return position;
 	}
 	
+	//Adds a new number but to the end of the list
+	public void append(int newNum) {
+		//Increases the size of the array if needed
+		if (count == currentListSize) {
+			currentListSize *= INCREASE_AMT;
+			int[] tempList = new int[currentListSize];
+			
+			for (int index = 0; index < count; index++) {
+				tempList[index] = list[index];
+			}
+			
+			list = tempList;
+		}
+		
+		list[count] = newNum;
+		count++;
+	}
+	
+	//returns the fist element in the list
+	public int first() {
+		return list[0];
+	}
+	
+	//returns the current size of the list array
+	public int size() {
+		return currentListSize;
+	}
 	
 	//Used for testing only
-	public int[] getArr()
-	{
+	public int[] getArr() {
 		return list;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
